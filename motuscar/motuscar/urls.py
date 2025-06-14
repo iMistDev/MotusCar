a116ce2bf0b0c53a9bd8b818090d32691fbfc2e0
iMistDev <agustinaam2001@gmail.com>
2025-06-14 18:07:10 -0400

    integraciones homepage

--- motuscar/motuscar/urls.py
+++ motuscar/motuscar/urls.py
@@ -27,7 +27,7 @@ def home(request):
 
 urlpatterns = [
     
-    path('', include('homepage.urls')),
+    path('home/', include('homepage.urls')),
     path('core/', include('core.urls')),
     path('admin/', admin.site.urls),
     path('login/', include('Login.urls')),
