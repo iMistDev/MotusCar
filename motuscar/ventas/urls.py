cb29326085663a1cfef7d42832c7e0c4251d33e7
iMistDev <agustinaam2001@gmail.com>
2025-08-27 19:40:46 -0400

    Cambios Template y Urls Ventas

--- motuscar/ventas/urls.py
+++ motuscar/ventas/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('productos/', views.lista_productos, name='lista_productos'),
+    path('productos/', views.lista_productos, name='lista_productos_repuestos'),
     path('producto/<int:producto_id>/', views.detalle_producto, name='detalle_producto'),
     path('agregar/<int:producto_id>/', views.agregar_al_carrito, name='agregar_al_carrito'),
     path('carrito/', views.ver_carrito, name='ver_carrito'),
